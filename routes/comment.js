748b67fd4487415923c95cf11ab1e0655b69e302
qsg1215 <1184053575@qq.com>
2018-08-27 22:35:36 +0800

    some change

--- routes/comment.js
+++ routes/comment.js
@@ -9,7 +9,7 @@ var util = require('../public/lib/util');
 var commentSQL = require('../db/SQL/commentSQL');
 
 //添加文章评论
-router.post('/new', function(req, res, next){
+router.post('/new', function(req, res){
     var commentInfo = req.body;
     commentInfo.ID =  uuid.v1();
         client.query(commentSQL.insert,
